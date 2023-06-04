e4793a959fe56f7e994df5154fb952d7662d366f
crumbtoo <crumb@duck.com>
2023-06-04 14:19:24 -0600

    throw err when $TOKEN is undefined

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,8 @@ client.once(Events.ClientReady, c => {
 	console.log(`hi ${c.user.tag}`);
 });
 
-client.login(process.env["TOKEN"]);
+if(process.env["TOKEN"] === undefined)
+	throw "set client token w/ $TOKEN (including the \"Bot \")"
+else
+	client.login(process.env["TOKEN"]);
 
